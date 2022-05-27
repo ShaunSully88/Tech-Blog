aaad2b2c07f53518a4e75215160a00e82f002c12
Shaun Sullivan <shaunsully88@gmail.com>
2022-05-27 15:55:25 -0400

    rearranged server.js file

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
+const routes = require('./controllers')
+const sequelize = require('./config/connection');
+
+const helpers = require('./utils/helpers');
+
 const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -19,18 +26,14 @@ const sess = {
   })
 };
 
+
 app.use(session(sess));
-
-const helpers = require('./utils/helpers');
-
-const hbs = exphbs.create({ helpers });
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(require('./controllers/'));
 
