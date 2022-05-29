70ff987e6f39d12b5f7ce99aeff599400911f150
Shaun Sullivan <shaunsully88@gmail.com>
2022-05-29 09:23:31 -0400

    Still same issue, it looks like issue lies in syncing with login.js page. Reaching out to askbcs

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const routes = require('./controllers')
 const sequelize = require('./config/connection');
 const path = require('path');
+const session = require('express-session');
 
 const helpers = require('./utils/helpers');
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({helpers});
-
-const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -26,14 +24,17 @@ const sess = {
   })
 };
 
-
 app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+
 
 app.use(require('./controllers/'));
 
